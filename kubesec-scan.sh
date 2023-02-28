4313a89dd1f7ddf095b659f852367199c80d33ea
rupesh1264 <rupeshb.tirupati@gmail.com>
2023-02-28 12:49:19 +0530

    added kubesec scan for k8s

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#kube-scan.sh
+#kubesec-scan.sh
 
 # using kubesec v2 ap1
 scan_result=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan | jq .|0|.message -r )
-scan_score=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan | jq .|0|.score )
+scan_message=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
+scan_score=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan | jq .[0].score )
 
 # using kubesec docker image for scanning
 # scan_result=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https://v2.kubesec.io/scan)
@@ -14,7 +14,7 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deploymeny_service.yaml" https:/
 
 
        # kubesec scan result processing
-       # echo "Scan Score : $Scan_score"
+       # echo "Scan Score : $scan_score"
 
        if [[ "${scan_score}" -ge 5]]; then
            echo "Score is $scan_score"
